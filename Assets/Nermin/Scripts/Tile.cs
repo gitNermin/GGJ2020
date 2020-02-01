34428fa495bfd5f4d2c2cffc83699772a631cdfe
NerminShawkat <shawkat.nermin@gmail.com>
2020-02-01 21:29:51 +0200

    tile

--- Assets/Nermin/Scripts/Tile.cs
+++ Assets/Nermin/Scripts/Tile.cs
@@ -13,10 +13,20 @@ public class Tile : MonoBehaviour
         Fixing
     }
 
-    public static UnityEvent<Tile> OnNonBatee5aBroken;
+    [SerializeField]
+    float _highlightTime;
+    
+    public static TileEvent OnNonBatee5aBroken = new TileEvent();
+    public static TileEvent OnTileFixed = new TileEvent();
+    public static TileEvent OnTileBroke = new TileEvent();
 
     public State _myState = State.Fixed;
 
+    private Renderer _myRenderer;
+
+
+    public float BreakTime;
+    public float FixTime;
 
     //gwa el pattern
     [SerializeField]
@@ -26,6 +36,16 @@ public class Tile : MonoBehaviour
         get
         {
             return _isBatee5a;
+        }
+    }
+
+    private void Start()
+    {
+        _myRenderer = GetComponent<Renderer>();
+        if (_isBatee5a)
+        {
+            transform.tag = Tags.Batee5a;
+            Highlight();
         }
     }
 
@@ -44,15 +64,45 @@ public class Tile : MonoBehaviour
 
     public void Fix()
     {
-
+        Debug.Log("fix");
+        StartCoroutine("FixCoroutine");
     }
+
+    public IEnumerator FixCoroutine()
+    {
+        _myState = State.Fixing;
+        yield return new WaitForSeconds(FixTime);
+        _myState = State.Fixed;
+    }
+
+
     public void Break()
     {
         if(!_isBatee5a)
         {
             OnNonBatee5aBroken.Invoke(this);
         }
+        Debug.Log("break");
+    }
+
+
+    IEnumerator BreakCoroutine()
+    {
+        _myState = State.Breaking;
+        yield return new WaitForSeconds(BreakTime);
+        _myState = State.Broken;
+    }
+
+    public void Highlight()
+    {
+        _myRenderer.material.color = Color.red;
+        Invoke("Dehighlight", _highlightTime);
+    }
+
+    public void Dehighlight()
+    {
+        _myRenderer.material.color = Color.white;
     }
 }
 
-public class TileEvent<Tile> { }
+public class TileEvent:UnityEvent<Tile> { }
