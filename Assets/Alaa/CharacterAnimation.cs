03cfcbc622d16325a9d8b6ed0880917db2a3fccb
Alaa Hatata <alaa.mohamed.hatata@gmail.com>
2020-02-02 15:10:49 +0200

    scenes

---
+++ Assets/Alaa/CharacterAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    Animator myAnimation;
+
+    void Start()
+    {
+        myAnimation = GetComponent<Animator>();
+    }
+    public void Walk()
+    {
+        myAnimation.SetTrigger("Walk");
+    }
+    public void Idle()
+    {
+        myAnimation.SetTrigger("Idle");
+    }
+    public void Shout()
+    {
+        myAnimation.SetTrigger("Shout");
+    }
+    public void Break()
+    {
+        myAnimation.SetTrigger("Break");
+    }
+}
