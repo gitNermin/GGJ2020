f1dcb50760dfd795bdf483ab83be1c700c33f46b
Alaa Hatata <alaa.mohamed.hatata@gmail.com>
2020-02-02 13:12:05 +0200

    assets

---
+++ Assets/Alaa/LoadingBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingBar : MonoBehaviour
+{
+    GameObject loadingCanvas;
+    [SerializeField]
+    GameObject loading;
+    Image loadingImage;
+    [SerializeField]
+    Vector3 offset;
+    private float startTime;
+    private float loadingTime;
+    public void ShowLoadingBar(float time,Vector3 position)
+    {
+        startTime = time;
+        loadingCanvas.transform.position = position + offset;
+        loadingCanvas.SetActive(true);
+    }
+    private void OnEnable()
+    {
+        startTime = Time.time;
+        loadingImage = loading.GetComponent<Image>();
+        loadingImage.fillAmount = 0;
+    }
+    IEnumerator LoadingEffect()
+    {
+        yield return null;
+        while (startTime + loadingTime < Time.time)
+        {
+            if (loadingImage.fillAmount == 1)
+                loadingCanvas.SetActive(false);
+        }
+
+    }
+
+}
